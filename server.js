bd2e1f2090def8b5f3e9b45ea21efac037092972
Uzias Rivera <uziasrivera@gmail.com>
2019-10-30 10:05:28 -0700

    challenge commit

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet')
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+function dateLogger(req,res,next){
+  console.log( new Date().toISOString())
+  console.log('HTTP METHOD: ',req.method,' URL: ',req.originalUrl)
+  next();
+}
+
+server.use(helmet()) // server.use(helmet(), express.json())
+// global middleware
+server.use(express.json()); // built in
+server.use(dateLogger) // customer middleware, currently not invoked)
 
 server.use('/api/hubs', hubsRouter);
 
